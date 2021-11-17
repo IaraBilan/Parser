ac921d02313d16b537527feadb6a9d81823311c7
Iaroslavna Khodyreva <yaroslavnaz@gmail.com>
2021-11-17 15:20:24 +0300

    console

---
+++ ConsoleApp5/GetDeals.ps1
@@ -0,0 +1,21 @@
+﻿$session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+$session.UserAgent = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36"
+$r = Invoke-WebRequest -UseBasicParsing -Uri "https://www.lesegais.ru/open-area/graphql" `
+-Method "POST" `
+-WebSession $session `
+-Headers @{
+"sec-ch-ua"="`" Not A;Brand`";v=`"99`", `"Chromium`";v=`"96`", `"Google Chrome`";v=`"96`""
+  "Accept"="*/*"
+  "sec-ch-ua-mobile"="?1"
+  "sec-ch-ua-platform"="`"Android`""
+  "Origin"="https://www.lesegais.ru"
+  "Sec-Fetch-Site"="same-origin"
+  "Sec-Fetch-Mode"="cors"
+  "Sec-Fetch-Dest"="empty"
+  "Referer"="https://www.lesegais.ru/open-area/deal"
+  "Accept-Encoding"="gzip, deflate, br"
+  "Accept-Language"="ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,uk;q=0.6"
+} `
+-ContentType "application/json" `
+-Body "{`"query`":`"query SearchReportWoodDeal(`$size: Int!, `$number: Int!, `$filter: Filter, `$orders: [Order!]) {\n  searchReportWoodDeal(filter: `$filter, pageable: {number: `$number, size: `$size}, orders: `$orders) {\n    content {\n      sellerName\n      sellerInn\n      buyerName\n      buyerInn\n      woodVolumeBuyer\n      woodVolumeSeller\n      dealDate\n      dealNumber\n      __typename\n    }\n    __typename\n  }\n}\n`",`"variables`":{`"size`":77,`"number`":0,`"filter`":null,`"orders`":null},`"operationName`":`"SearchReportWoodDeal`"}"
+ $r.RawContent
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Text.Json;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using DAL;
+using System.Linq;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start getting data...");
+            ParseAndSaveDataToDB();
+            Console.ReadLine();
+        }
+
+        static void ParseAndSaveDataToDB()
+        {
+            try
+            {
+                var dal = new DealsDAL();
+                int dealsTotalCount = GetTotalDealsCount();
+                Console.WriteLine("Total amount of deals on portal: {0}", dealsTotalCount);
+                int dealsCountInDB = dal.GetDealsCount();
+                Console.WriteLine("Total amount of deals in DB: {0}", dealsCountInDB);
+
+                //check for new deals
+                if (dealsTotalCount > dealsCountInDB)
+                {
+                    Console.WriteLine("Start getting new data from portal...");
+                    var dealNumberFromDB = dal.GetDealNumbers();
+                    string data = RunPowerShellScript(@"C:\Users\DOTNET\source\repos\ConsoleApp5\GetDeals.ps1", dealsTotalCount);
+                    var json = data.Substring(data.IndexOf("content") + 9, data.Length - data.IndexOf("content") - 48);
+
+                    //parse json and save to db
+                    using var doc = JsonDocument.Parse(json);
+                    JsonElement root = doc.RootElement;
+
+                    var deals = root.EnumerateArray();
+                    while (deals.MoveNext())
+                    {
+                        var deal = deals.Current;
+                        var props = deal.EnumerateObject();
+
+                        while (props.MoveNext())
+                        {
+                            var prop = props.Current;
+                            if (prop.Name == "dealNumber")
+                            {
+                                decimal dealNumber = Convert.ToDecimal(prop.Value.ToString());
+                                if (dealNumberFromDB.Where(n => n == Convert.ToDecimal(prop.Value.ToString())).Count() == 0)
+                                {
+                                    dal.AddDeal(new Deal() { DealNumber = dealNumber, Data = deal.ToString() });
+                                    Console.WriteLine("Deal with number {0} has been added to DB", dealNumber);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Deal with number {0} is already in DB", dealNumber);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.InnerException);
+            }
+        }
+
+        static string RunPowerShellScript(string scriptPath, int dealsAmount = 20)
+        {
+            var script = File.ReadAllText(scriptPath);
+            if (dealsAmount != 20)
+                script = script.Replace(":77", ":" + dealsAmount);
+
+            // Create a runspace.
+            using (Runspace myRunSpace = RunspaceFactory.CreateRunspace())
+            {
+                myRunSpace.Open();
+                using (PowerShell powershell = PowerShell.Create())
+                {
+                    // Create a pipeline with the Get-Command command.
+                    powershell.AddScript(script);
+
+                    // execute the script
+                    var results = powershell.Invoke();
+                    powershell.Streams.ClearStreams();
+                    powershell.Commands.Clear();
+                    // convert the script result into a single string
+                    StringBuilder stringBuilder = new StringBuilder();
+                    foreach (PSObject obj in results)
+                    {
+                        stringBuilder.AppendLine(obj.ToString());
+                    }
+
+                    return stringBuilder.ToString();
+                }
+            }
+        }
+
+        static int GetTotalDealsCount()
+        {
+            string data = RunPowerShellScript(@"C:\Users\DOTNET\source\repos\ConsoleApp5\GetTotal.ps1");
+
+            int totalEnd = data.IndexOf("number") - 2;
+            int totalStart = data.IndexOf("total") + 7;
+            var totalDealsCout = Convert.ToInt32(data.Substring(totalStart, totalEnd - totalStart));
+            return totalDealsCout;
+        }
+    }
+}
